d66963d7365d97147e5f15a50f37fbeccbb06f0e
Ivan Silva <42705523+ioouj@users.noreply.github.com>
2024-01-17 21:25:16 -0600

    misc

--- emacs.el
+++ emacs.el
@@ -6,7 +6,32 @@
   ;; Emacs configuration file content is written below.
 
   (setq default-frame-alist '((width . 100) (height . 40))) ;; Window size
-  (load-theme 'modus-vivendi-deuteranopia)
+
+
+
+  (setq modus-themes-italic-constructs t)
+  (setq modus-themes-common-palette-overrides
+	'(
+	  (bg-mode-line-active "#003333")
+	  (border-mode-line-active "#556666")
+	  (bg-region bg-sage)
+	  (builtin magenta)
+	  (comment "#7A4E54")
+	  ;;(constant magenta-cooler)
+	  (constant magenta-cooler)
+;;	  (docstring green-faint)
+	  (docmarkup magenta-faint)
+	  (fnname magenta-warmer)
+	  (keyword cyan)
+	  (preprocessor magenta-warmer)
+	  (string blue-cooler)
+	  (type magenta-cooler)
+	  (variable cyan-faint)
+	  (rx-construct magenta-warmer)
+	  (rx-backslash blue-cooler)
+	  ))
+  (load-theme 'modus-vivendi-tritanopia)
+  
   (setq cursor-type 'bar)
   (setq inhibit-startup-screen t)
 
@@ -23,8 +48,6 @@
   (recentf-mode 1)
   (delete-selection-mode +1)
   (global-goto-address-mode +1)
-  (global-visual-line-mode +1)
-  (move-text-default-bindings)
   (electric-pair-mode t)
 
   ;; melpa
@@ -64,16 +87,18 @@
 			 "*Buffer List*"
 			 "*clang-error*"			 
 			 "*clang-output*"
+			 (seq "*clangd" (+ anything))
 			 "*compilation*"
 			 "*Compile-Log*"
 			 "*Completions*"
 			 (seq "*EGLOT" (+ anything))
 			 (seq "*Flymake" (+ anything))
 			 "*Help*"
-			 "*lsp-log*"
+			 (seq "*lsp-" (+ anything))
 			 (seq "magit" (+ anything))
 			 "*Messages*"
 			 "*Packages*"
+			 "*pylsp*"
 			 "*scratch*"
 			 "*Shell Command Output*"
 			 (seq "*ts-ls" (+ anything))
@@ -134,10 +159,13 @@
   ;; Auto save
   (setq auto-save-file-name-transforms `((".*" "~/.emacs.d/auto-saved-files/" t)))
 
+  (setq dired-kill-when-opening-new-dired-buffer t)
+  (setq vc-follow-symlinks t)
+  (fset 'yes-or-no-p 'y-or-n-p)
+  
   ;; Files / buffers
   (global-set-key "\C-x\ \C-r" 'recentf-open-files)
   (global-set-key (kbd "C-w") 'kill-buffer)
-  
 
   (defun get-def-dir-status-cmd ()
     (format "git -C %s status 2>/dev/null" default-directory))
@@ -174,30 +202,54 @@
     (string-match-p hidden-buffers-regexp (buffer-name buffer)))
 
   (setq switch-to-prev-buffer-skip 'my-buffer-skip-p)
-
   
 
-  (defun load-company-hook ()
-    ;; (defun tab-indent-or-complete ()
-    ;;   (interactive)
-    ;;   (if (minibufferp)
-    ;;       (minibuffer-complete)
-    ;;     (if (or (not yas-minor-mode)
-    ;;             (null (do-yas-expand)))
-    ;;         (if (check-expansion)
-    ;;             (company-complete-common)
-    ;;           (indent-for-tab-command)))))
+  (use-package company
+    :ensure t
+    :config
+    (setq company-idle-delay 0
+	  company-minimum-prefix-length 2
+	  company-show-numbers t
+	  )    
+    (global-company-mode t)
+    )
 
-    ;; (global-set-key [backtab] 'tab-indent-or-complete)
-    
-    ;;(setq company-dabbrev-downcase 0)
-    ;;(setq company-idle-delay 0)
-    (company-mode 1)
-    (global-company-mode)
-    (setq company-minimum-prefix-length 2))
+  (use-package anaconda-mode
+    :ensure t
+    :config
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    ;;(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    )
 
+  (use-package company-anaconda
+    :ensure t
+    :init (require 'rx)
+    :after (company)
+    :config
+    (add-to-list 'company-backends 'company-anaconda)
+    )
+
+  (use-package company-quickhelp
+  ;; Quickhelp may incorrectly place tooltip towards end of buffer
+  ;; See: https://github.com/expez/company-quickhelp/issues/72
+  :ensure t
+  :config
+  (company-quickhelp-mode)
+  )
+
+
+  ;; copy working directory
+  (defun sjlwa/cpwd ()
+    (interactive)
+    (let ((wd (nth 1 (s-split "Directory " (pwd)))))
+      (shell-command (concat "echo " wd " | xclip -sel clip &> /dev/null"))
+      (prin1 (concat "Copied: " wd))))
+   
   
-  
+
+  ;;(global-set-key (kbd "M-s") 'compile)
+
+
   ;; programming
   (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
   (add-hook 'html-mode-hook 'emmet-mode)
@@ -207,6 +259,53 @@
   (add-to-list 'auto-mode-alist '("\\.erb?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.yml?\\'" . yaml-mode))
 
+  ;; rust
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+  (use-package rust-ts-mode
+    :ensure t
+    :hook ((rust-ts-mode . lsp-mode)
+	   (rust-ts-mode . company-mode)))
+
+ 
+  ;;
+  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+  (use-package go-mode
+    :ensure t
+    :hook ((go-mode . lsp-mode)
+	   (go-mode . company-mode)))
+
+
+
+
+
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (package-initialize)
+
+  (setq package-selected-packages 
+    '(dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company
+      ;; Optional packages
+      lsp-ui company hover))
+
+  (when (cl-find-if-not #'package-installed-p package-selected-packages)
+    (package-refresh-contents)
+    (mapc #'package-install package-selected-packages))
+
+  (add-hook 'dart-mode-hook 'lsp)
+
+  (setq gc-cons-threshold (* 100 1024 1024)
+	read-process-output-max (* 1024 1024))
+
+  (setq lsp-dart-sdk-dir "/home/sjlwa/snap/flutter/common/flutter/bin/cache/dart-sdk")
+  (setq lsp-dart-flutter-sdk "/home/sjlwa/snap/flutter/common/flutter/")
+  (setq flutter-sdk-path "/home/sjlwa/snap/flutter/common/flutter/")
+  
+
+
+
+  
+  
+  ;;
   (require 'lsp-mode)
   (add-hook 'typescript-mode-hook #'lsp)
   (add-hook 'python-mode-hook #'lsp)
@@ -214,21 +313,7 @@
 
   (global-set-key [backtab] 'emmet-expand-line)
   
-  (add-hook 'c-mode-hook 'load-company-hook)
-  (add-hook 'python-mode-hook 'load-company-hook)
-  (add-hook 'emacs-lisp-mode-hook 'load-company-hook)
 
-  ;; (setq treesit-language-source-alist
-  ;; 	'((tsx  ;; 	   "https://github.com/tree-sitter/tree-sitter-typescript"
-  ;; 	   "master"
-  ;; 	   "tsx/src")
-	  
-  ;; 	  (typescript
-  ;; 	   "https://github.com/tree-sitter/tree-sitter-typescript"
-  ;; 	   "master"
-  ;; 	   "typescript/src")))
-  
-  
   (add-to-list 'load-path "~/.emacs.d/tsx-mode/")
   (setq typescript-indent-level 2) 
   
@@ -239,6 +324,8 @@
   
   ;; recursive search
   (global-set-key (kbd "C-S-s") 'ag)
+
+  (global-set-key (kbd "C-s") 'occur)
 
   ;; Scale text
   (global-set-key (kbd "C-+") 'text-scale-increase)
@@ -255,7 +342,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-mode treemacs lsp-treemacs http company dired-sidebar expand-region markdown-mode typescript-mode web-mode yaml-mode)))
+   '(lsp-dart dart-mode go-mode pdf-tools edbi company-quickhelp arduino-mode lsp-mode treemacs lsp-treemacs http company dired-sidebar expand-region markdown-mode typescript-mode web-mode yaml-mode)))
 
 
 (custom-set-faces
