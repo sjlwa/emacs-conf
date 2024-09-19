141f1fd46b1dd21e62d65701e39774cf57034ea7
sjlwa <ivansilva.me@gmail.com>
2024-09-19 09:16:38 -0600

    `?` Remove company-omnisharp loading just after company, enables it if company is active.

--- langs.el
+++ langs.el
@@ -32,7 +32,8 @@
 (defun load-lang--csharp ()
   "Loads the required configuration for csharp mode"
   (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode))
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-omnisharp))
+  (if (bound-and-true-p company-mode)
+      (add-to-list 'company-backends 'company-omnisharp))
   
   (defun csharp-init-modes ()
     (flycheck-mode)
