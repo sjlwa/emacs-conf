73d1b79c1fb23d6472980db6631e53abb7c8477b
sjlwa <ivansilva.me@gmail.com>
2024-12-16 21:09:48 -0600

    Customize company and lsp general behaviour

--- ide.el
+++ ide.el
@@ -2,10 +2,16 @@
   (setq company-idle-delay 0
         company-minimum-prefix-length 1
         company-show-numbers t
+        company-tooltip-width-grow-only t
+        company-tooltip-maximum-width 40
+        company-tooltip-minimum-width 40
         company-tooltip-offset-display 'lines
-        company-tooltip-align-annotations nil
-        company-transformers '(delete-consecutive-dups
-                               company-sort-by-occurrence))
+        company-tooltip-align-annotations nil)
+  
+  (unless (eq major-mode 'eshell-mode)
+    (setq company-transformers '(;; delete-consecutive-dups ;; TODO: Fix crash eshell with esh-autocomplete.
+                                 company-sort-by-occurrence)))
+
   (if (require 'company-quickhelp nil 'noerror)
       (company-quickhelp-mode)))
 
@@ -14,7 +20,9 @@
         lsp-enable-file-watchers nil
         lsp-diagnostics-flycheck-default-level 'info
         lsp-ui-sideline-diagnostic-max-line-length 150
-        lsp-ui-sideline-diagnostic-max-lines 3))
+        lsp-ui-sideline-diagnostic-max-lines 3
+        lsp-completion-show-detail nil
+        lsp-auto-guess-root t))
 
 
 (add-hook 'company-mode-hook 'company-init-config)
